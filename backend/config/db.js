2fd380199d6b0e935bb0a77821ba5c3aecf102a5
mohamed <hn4717064@gmail.com>
2026-06-11 12:18:26 +0300

    deploy mongo1

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
-  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/hucems_db';
+  const uri = process.env.MONGO_URI || 'mongodb://admin:Hhn4717064%40%40@127.0.0.1:27017/hu_system?authSource=admin';
   await mongoose.connect(uri);
   console.log(`MongoDB connected: ${mongoose.connection.host}/${mongoose.connection.name}`);
 };
